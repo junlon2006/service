948098bd3a2e187be9b0d3857c9c3b95e5bad4a0
shangjinlong <shangjinlong@unisound.com>
2019-11-07 18:31:18 +0800

    集成redisson，简易的实现redis做为mysql缓存，增加TPS，funny

--- src/main/java/com/github/junlon2006/microservice/controller/MicroController.java
+++ src/main/java/com/github/junlon2006/microservice/controller/MicroController.java
@@ -1,5 +1,6 @@
 package com.github.junlon2006.microservice.controller;
 
+import com.alibaba.fastjson.JSON;
 import com.github.junlon2006.microservice.data.Result;
 import com.github.junlon2006.microservice.entity.MicroTortoiseInfo;
 import com.github.junlon2006.microservice.service.MicroService;
@@ -7,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -27,6 +29,7 @@ public class MicroController {
 
     /**
      * get all tortoise information list from register database
+     *
      * @return
      */
     @GetMapping("/get-all")
@@ -36,13 +39,27 @@ public class MicroController {
 
     /**
      * set tortoise information list, and insert data to database
+     *
      * @param tortoiseInfos
      * @return
      */
-    @PostMapping(value = "set-list", produces = MediaType.APPLICATION_JSON_UTF8_VALUE,
+    @PostMapping(value = "/set-list", produces = MediaType.APPLICATION_JSON_UTF8_VALUE,
             consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public Result<?> setTortoiseInfo(@RequestBody @Validated List<MicroTortoiseInfo> tortoiseInfos) {
         return microService.setTortoiseList(tortoiseInfos);
+    }
+
+    @PostMapping(value = "/find-byname", produces = MediaType.APPLICATION_JSON_UTF8_VALUE,
+            consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public Result<?> getInfoByName(@RequestBody String name) {
+        return microService.getInfoByName(name);
+    }
+
+    @PostMapping(value = "/save-pic", produces = MediaType.APPLICATION_JSON_UTF8_VALUE,
+            consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public Result<?> saveTortoisePicture(@RequestPart("microTortoiseInfo") String microTortoiseInfo,
+                                         @RequestParam("picture") MultipartFile file) {
+        return microService.saveTortoisePicture(JSON.parseObject(microTortoiseInfo, MicroTortoiseInfo.class), file);
     }
 
 }
